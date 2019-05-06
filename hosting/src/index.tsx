b546fb026526efc74e3640442825b0d90d288631
kiiroitory <yoshiakis2002@gmail.com>
2019-05-06 19:38:29 +0900

    Todoを追加するコンポーネントを追加

--- hosting/src/index.tsx
+++ hosting/src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Header } from "./components/Header";
+import { Header } from "./main/views/header/Header";
+import { Todo } from "./views/todo/Todo";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Todo />
       </div>
     );
   }
---
+++ hosting/src/views/todo/Todo.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+export enum Priority {
+  High = 'high',
+  Middle = 'middle',
+  Low = 'low'
+}
+
+interface TodoState {
+  todo: string
+  priority: Priority
+}
+
+export class Todo extends React.Component<{}, TodoState> {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todo: '',
+      priority: Priority.Middle
+    }
+    this.changeInputTodo = this.changeInputTodo.bind(this)
+    this.changeInputPriority = this.changeInputPriority.bind(this)
+    this.show = this.show.bind(this)
+  }
+
+  changeInputTodo(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      todo: e.target.value
+      }
+    )
+  }
+
+  changeInputPriority(e: React.ChangeEvent<HTMLSelectElement>) {
+    this.setState({
+      priority: e.target.value as Priority
+    })
+  }
+
+  show() {
+    console.log(`todo: ${this.state.todo}`)
+    console.log(`priority: ${this.state.priority}`)
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type='text' name='todo' placeholder='タスクを入力' onChange={this.changeInputTodo}/>
+          <select value={this.state.priority} onChange={this.changeInputPriority}>
+            <option value={Priority.High}>高</option>
+            <option value={Priority.Middle}>中</option>
+            <option value={Priority.Low}>低</option>
+          </select>
+          <button type='button' onClick={this.show}>追加</button>
+        </form>
+      </div>
+    )
+  }
+}
